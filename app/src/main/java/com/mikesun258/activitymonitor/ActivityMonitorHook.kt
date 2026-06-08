d1e6e7cb1405fc67ab28886ad03609ae95dfc2af
mikesun258 <mikesun258@126.com>
2026-06-08 10:08:03 +0800

    Update ActivityMonitorHook.kt

--- app/src/main/java/com/mikesun258/activitymonitor/ActivityMonitorHook.kt
+++ app/src/main/java/com/mikesun258/activitymonitor/ActivityMonitorHook.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class ActivityMonitorHook : IXposedHookLoadPackage {
@@ -15,54 +15,40 @@ class ActivityMonitorHook : IXposedHookLoadPackage {
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         try {
+            val activityClass = lpparam.classLoader.loadClass("android.app.Activity")
+
             // 1. Hook Activity.onCreate
-            XposedHelpers.findAndHookMethod(
-                Activity::class.java,
-                lpparam.classLoader,
-                "onCreate",
-                Bundle::class.java,
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val act = param.thisObject as Activity
-                        val pkg = act.packageName
-                        val cls = act.javaClass.name
-                        Log.d(TAG, "OnCreate | $pkg / $cls")
-                        sendEventBroadcast(act, pkg, cls, "onCreate")
-                    }
+            XposedBridge.hookAllMethods(activityClass, "onCreate", object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val act = param.thisObject as Activity
+                    val pkg = act.packageName
+                    val cls = act.javaClass.name
+                    Log.d(TAG, "OnCreate | $pkg / $cls")
+                    sendEventBroadcast(act, pkg, cls, "onCreate")
                 }
-            )
+            })
 
             // 2. Hook Activity.onResume
-            XposedHelpers.findAndHookMethod(
-                Activity::class.java,
-                lpparam.classLoader,
-                "onResume",
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val act = param.thisObject as Activity
-                        val pkg = act.packageName
-                        val cls = act.javaClass.name
-                        Log.d(TAG, "OnResume | $pkg / $cls")
-                        sendEventBroadcast(act, pkg, cls, "onResume")
-                    }
+            XposedBridge.hookAllMethods(activityClass, "onResume", object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val act = param.thisObject as Activity
+                    val pkg = act.packageName
+                    val cls = act.javaClass.name
+                    Log.d(TAG, "OnResume | $pkg / $cls")
+                    sendEventBroadcast(act, pkg, cls, "onResume")
                 }
-            )
+            })
 
             // 3. Hook Activity.onPause
-            XposedHelpers.findAndHookMethod(
-                Activity::class.java,
-                lpparam.classLoader,
-                "onPause",
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val act = param.thisObject as Activity
-                        val pkg = act.packageName
-                        val cls = act.javaClass.name
-                        Log.d(TAG, "OnPause | $pkg / $cls")
-                        sendEventBroadcast(act, pkg, cls, "onPause")
-                    }
+            XposedBridge.hookAllMethods(activityClass, "onPause", object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val act = param.thisObject as Activity
+                    val pkg = act.packageName
+                    val cls = act.javaClass.name
+                    Log.d(TAG, "OnPause | $pkg / $cls")
+                    sendEventBroadcast(act, pkg, cls, "onPause")
                 }
-            )
+            })
 
         } catch (e: Throwable) {
             Log.e(TAG, "Hook failed for package: ${lpparam.packageName}", e)
